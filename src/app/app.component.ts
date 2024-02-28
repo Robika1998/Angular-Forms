f3333eba41986f9b920f319bbce1fbe579fffd0e
Robika1998 <robika.beglarashvili@mail.ru>
2024-02-28 17:15:47 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface IsalesTaxArray {
+  id: number,
+  title: string,
+  value: number
+}
+
+interface items {
+  id: number,
+  name:string,
+  items: [{
+    id: number,
+    name: string,
+    glCode: number,
+    amount: number,
+    salesTax: IsalesTaxArray;
+  }];
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +25,42 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular-Forms';
+
+  itemsTitles = [
+    'Category',
+    'Items',
+    'GL Code',
+    'Amount',
+    'Sales Tax',
+    'Total'
+  ]
+
+  localItems: items[] = [
+    {
+      id:1,
+      name: 'Category 1',
+      items: [{
+        id: 1,
+        name: 'Item 1',
+        glCode: 0,
+        amount: 0,
+        salesTax: {id: 0, title: '', value: 0},
+      }]
+    }
+  ];
+
+  salesTaxArray: IsalesTaxArray[] = [
+    {id: 1, title: 'No Tax', value: 0},
+    {id: 2, title: 'Georgia', value: 10},
+    {id: 3, title: 'Germany', value: 20},
+  ];
+
+  ngOnInit() {
+    this.localItems[0].items[0].salesTax = this.salesTaxArray[0];
+  }
+
+  submitForm(myForm: NgForm) {
+     console.log(myForm);
+     
+  }
 }
